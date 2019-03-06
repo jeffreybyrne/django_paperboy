9adb915c410d05dfeebfcd117c4c6ece22832fd0
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-03-06 12:21:47 -0500

    added more functionality to the paperboy model, still need to add the class methods

---
+++ django_paperboy/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django_paperboy.models import Paperboy
+
+admin.site.register(Paperboy)
---
+++ django_paperboy/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from math import floor
+
+
+class Paperboy(models.Model):
+    name = models.CharField(max_length=255)
+    experience = models.IntegerField()
+    earnings = models.FloatField()
+
+    def __str__(self):
+        return "This paperboy is named {}. He has delivered {} papers and earned ${:.2f}.".format(self.name, self.experience, self.earnings)
+
+    def quota(self):
+        return floor(self.experience * 0.5 + 50)
+
+    def report(self):
+        return "Look at me! I'm {} and I've delivered {} papers so far. They've paid me ${:.2f} for this, can you believe it?!".format(self.name, self.experience, self.earnings)
+
+    def deliver(self, start_address, end_address):
+        start = min(start_address, end_address)
+        end = max(start_address, end_address)
+        total_houses = end - start + 1
+        curr_quota = self.quota()
+        if total_houses == curr_quota:
+            earned = (0.25 * total_houses)
+        elif total_houses < curr_quota:
+            earned = (0.25 * total_houses) - 2
+        elif total_houses > curr_quota:
+            earned = (0.25 * curr_quota) + (0.50 * (total_houses-curr_quota))
+        self.experience += total_houses
+        self.earnings += earned
+        self.save()
+        return earned
